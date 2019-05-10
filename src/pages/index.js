cbc2a561df268859b2bbd672c775e2a1b9a3018c
ede <44153717+elvispressler@users.noreply.github.com>
2019-05-10 20:42:47 +0200

    1

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ class HomeIndex extends React.Component {
     }
 
     render() {
-        const siteTitle = "Gatsby Starter - Strata"
+        const siteTitle = "elvispresser"
         const siteDescription = "Site description"
 
         return (
@@ -89,16 +89,17 @@ class HomeIndex extends React.Component {
 
                     <section id="one">
                         <header className="major">
-                            <h2>Ipsum lorem dolor aliquam ante commodo<br />
-                            magna sed accumsan arcu neque.</h2>
+                            <h1>Erich Florow</h1>
                         </header>
-                        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
-                        <ul className="actions">
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
+                        <p>IT Service Manager</p>
+
                     </section>
 
                     <section id="two">
+
+                    <ul className="actions">
+                        <li><a href="#" className="button">Learn More</a></li>
+                    </ul>
                         <h2>Recent Work</h2>
 
                         <Gallery images={DEFAULT_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
